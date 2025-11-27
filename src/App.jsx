d58db533939b5d81cb9896954e7ff537d63998ad
Alana Micheal <alanaeniola@gmail.com>
2025-11-28 00:24:23 +0100

    This is the installation of React and Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='bg-red-500 p-4 text-center text-white'>Welcome to react</h1>
+    </div>
+  )
+}
+
+export default App
